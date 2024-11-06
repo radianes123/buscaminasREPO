ce188e1fec32b279f0a0463086878bcf36ae201f
Max <m.olivera02@ufromail.cl>
2024-11-06 12:13:07 -0300

    Update buscaminasC.py

--- buscaminasC.py
+++ buscaminasC.py
@@ -13,7 +13,15 @@ class field:
     def __init__(self,cols,rows):
         self.cols=cols
         self.rows=rows
-        self.gameField=np.zeros(cols,rows) # Crear matriz de ceros
+        self.gameField=np.zeros((rows,cols)) # Crear matriz de juego
+        self.playField=np.zeros((rows,cols)) # Crear matriz de jugadas
 
-    def generateField(self):
+    def generateField(self,bombs):
+        for i in range(bombs-1):
+            excluded=[] # Elementos excluídos para que no se repitan
+            if i==0:
+                excluded.append(ra.randint(0,self.rows-1),ra.randint(0,self.cols-1))
+            else:
+                for j in excluded:
+                    if excluded
         
