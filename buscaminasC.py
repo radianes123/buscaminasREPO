cf31ea8b82834528e8bc92a4da60d27b15a79781
asthala1 <l.zavala02@ufromail.cl>
2024-11-06 12:16:14 -0300

    buscaminasC.py
    Se agregó la clase Menu, que contiene los botones para iniciar un Juego fácil, intermedio y difícil, como también un botón para salir.

--- buscaminasC.py
+++ buscaminasC.py
@@ -25,3 +25,26 @@ class field:
                 for j in excluded:
                     if excluded
         
+class Menu:
+    def __init__(self,screen):
+        self.screen = screen
+        self.font = pygame.font.SysFont("Visitor TT1 BRK", 30)
+        self.buttons = [
+            {"text": "Fácil", "rect": pygame.Rect(100,50,200,80), "action": self.start_easy_game}
+            {"text": "Intermedio", "rect": pygame.Rect(100,150,200,80), "action": self.start_medium_game}
+            {"text": "Difícil", "rect": pygame.Rect(100,250,200,80), "action": self.start_hard_game}
+            {"text": "Salir", "rect": pygame.Rect(100,350,200,80), "action": self.quit_game}
+        ]
+
+    def start_easy_game(self):
+        Game(self.screen, "Fácil").run()
+
+    def start_medium_game(self):
+        Game(self.screen, "Intermedio").run()
+
+    def start_hard_game(self):
+        Game(self.screen, "Difícil").run()
+
+    def quit_game(self):
+        pygame.quit()
+        sys.exit()
