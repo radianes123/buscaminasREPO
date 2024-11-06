de10f0e90b32b891261a9b2f85ce6691a2457c58
Max <m.olivera02@ufromail.cl>
2024-11-06 13:56:46 -0300

    Update buscaminasC.py

--- buscaminasC.py
+++ buscaminasC.py
@@ -39,15 +39,41 @@ class field:
                         excluded.append(bombpos)
                         self.gameField[bombpos[0],bombpos[1]]="b"
                         k+=1
-    def detectBomb(self,col,row):
-        if self.gameField[row,col]=="b":
+    class field:
+    def __init__(self,cols,rows):
+        self.cols=cols
+        self.rows=rows
+        self.gameField=np.zeros((rows,cols)) # Crear matriz de juego
+    def __repr__(self):
+        return f'{self.gameField}'
+    def generateField(self,bombs):
+        excluded=[] # Lista de elementos excluídos para que no se repitan
+        for i in range(bombs):
+            if i==0: # Si es la primer bomba, se salta el proceso de verificación
+                bombpos=[random.randint(0,self.rows-1),random.randint(0,self.cols-1)] # Posición de la bomba
+                excluded.append(bombpos) # Excluir dicha posición
+                self.gameField[bombpos[0],bombpos[1]]=101 # Colocar la bomba en la matriz de juego
+            else:
+                k=0
+                while True:
+                    fail=0
+                    bombpos=[random.randint(0,self.rows-1),random.randint(0,self.cols-1)]
+                    for j in range(len(excluded)):
+                        if bombpos[0]==excluded[j][0] and bombpos[1]==excluded[j][1]:
+                            fail+=1
+                    if fail==0:
+                        excluded.append(bombpos)
+                        self.gameField[bombpos[0],bombpos[1]]=101
+                        break
+    def detectBomb(self,row,col):
+        if self.gameField[row,col]==101:
             return "b"
         else:
             bombs=0
-            for i in range(-1,1,1):
-                for j in range(-1,1,1):
-                    if col+i>=0 and col+i<=self.cols and row+j>=0 and row+j<=self.rows:
-                        if self.gameField[row+i,col+i]=="b":
+            for i in range(-1,2,1):
+                for j in range(-1,2,1):
+                    if col+j>=0 and col+j<=self.cols and row+i>=0 and row+i<=self.rows:
+                        if self.gameField[row+i,col+j]==101:
                             bombs+=1
             return bombs
 class Menu:
